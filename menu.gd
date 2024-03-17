e5cf60ec2d6749b8d0fe26952f5bd3825ba5931f
mkseay2 <mkseay2@illinois.edu>
2024-03-17 02:22:32 -0500

    initial main menu and level 1 setup

---
+++ menu.gd
@@ -0,0 +1,20 @@
+class_name MainMenu
+extends Control
+
+@onready var new_game_button = $VBoxContainer/NewGameButton as Button
+@onready var resume_game_button = $VBoxContainer/ResumeGameButton as Button
+@onready var exit_game_button = $VBoxContainer/ExitGameButton as Button
+@onready var start_level = preload("res://Levels/level_1.tscn") as PackedScene
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	new_game_button.button_down.connect(on_new_game_pressed)
+	exit_game_button.button_down.connect(on_exit_pressed)
+
+
+func on_new_game_pressed() -> void:
+	get_tree().change_scene_to_packed(start_level)
+
+
+func on_exit_pressed() -> void:
+	get_tree().quit()
