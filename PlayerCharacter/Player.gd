50230812111ce596edcc4f1f8e52ea28ef39807b
mkseay2 <mkseay2@illinois.edu>
2024-03-29 01:54:39 -0500

    added character movement

---
+++ PlayerCharacter/Player.gd
@@ -0,0 +1,12 @@
+extends CharacterBody2D
+
+@export var speed = 100
+
+func get_input():
+	# uses the four arrow keys
+	var input_direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	velocity = input_direction * speed
+
+func _physics_process(_delta):
+	get_input()
+	move_and_slide()
