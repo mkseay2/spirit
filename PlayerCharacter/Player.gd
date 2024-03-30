a580d69b80fbee3c52a56eef785eae714dc4ae48
mkseay2 <mkseay2@illinois.edu>
2024-03-30 18:16:26 -0500

    added player collision and smoother movement

--- PlayerCharacter/Player.gd
+++ PlayerCharacter/Player.gd
@@ -5,7 +5,7 @@ extends CharacterBody2D
 func get_input():
 	# uses the four arrow keys
 	var input_direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	velocity = input_direction * speed
+	velocity = input_direction.normalized() * speed
 
 func _physics_process(_delta):
 	get_input()
