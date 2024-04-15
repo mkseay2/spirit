c783f1e7ad24bd2f7f609f602e38b34f1764fe71
mkseay2 <mkseay2@illinois.edu>
2024-04-15 00:26:18 -0500

    completed level3 connections

--- Levels/level3/level3_room3.gd
+++ Levels/level3/level3_room3.gd
@@ -1,11 +1,28 @@
 extends Node2D
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	if Global.transition_backwards == false:
+		$Player.global_transform.origin.x = 25
+		$Player.global_transform.origin.y = 155
+	else:
+		$Player.global_transform.origin.x = 416
+		$Player.global_transform.origin.y = 155
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	pass
+	change_scene()
+
+
+func _on_room_2_transition_body_entered(body):
+	if body.has_method("player"):
+		Global.transition_scene = true
+		Global.transition_backwards = true
+
+
+func change_scene():
+	if Global.transition_scene == true:
+		if Global.current_scene == "level3_room3":
+			if Global.transition_backwards == true:
+				get_tree().change_scene_to_file("res://Levels/level3/level3_room2.tscn")
+				Global.finish_change_scene("level3_room2")
