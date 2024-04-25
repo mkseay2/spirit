28d62f4fb87818e4caa3ae39f4f22ce589de5080
CollinFox <cdfox3@illinois.edu>
2024-04-25 12:53:05 -0500

    poob

--- UI/colorPasswordScreen.gd
+++ UI/colorPasswordScreen.gd
@@ -1,11 +1,48 @@
+class_name colorPassword
 extends Control
 
+var blueCount = 0
+var greenCount = 0
+var yellowCount = 0
+var redCount = 0
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
+@onready var blueCountLabel
+@onready var redCountLabel
+@onready var greenCountLabel
+@onready var yellowCountLabel
+
+func updateBlueCount():
+	blueCount += 1
+	blueCountLabel.text = "%d" % blueCount
+
+
+func updateRedCount():
+	redCount += 1
+	redCountLabel.text = "%d" % redCount
+
+func updateGreenCount():
+	greenCount += 1
+	greenCountLabel.text = "%d" % greenCount
+
+func updateYellowCount():
+	yellowCount += 1
+	yellowCountLabel.text = "%d" % yellowCount
+
+	
+
+
+func _on_yellow_button_pressed():
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+
+func _on_red_button_pressed():
+	pass # Replace with function body.
+	
+
+func _on_green_button_pressed():
+	pass # Replace with function body.
+
+
+func _on_blue_button_pressed():
+	pass # Replace with function body.
