62fc3a756145be61d64c44e4732ca421aa386924
CollinFox <cdfox3@illinois.edu>
2024-04-25 13:34:20 -0500

    Update colorPasswordScreen.gd

--- UI/colorPasswordScreen.gd
+++ UI/colorPasswordScreen.gd
@@ -6,27 +6,34 @@ var greenCount = 0
 var yellowCount = 0
 var redCount = 0
 
+var blueFlag = 0
+var greenFlag = 0
+var redFlag = 0
+var yellowFlag = 0
+
+
 @onready var blueCountLabel = $ColorPasswordScreen/TextureRect2/ColorDisplay/ColorUI/BlueLabel
 @onready var redCountLabel = $ColorPasswordScreen/TextureRect2/ColorDisplay/ColorUI/redLabel
 @onready var greenCountLabel = $ColorPasswordScreen/TextureRect2/ColorDisplay/ColorUI/GreenLabel
 @onready var yellowCountLabel = $ColorPasswordScreen/TextureRect2/ColorDisplay/ColorUI/YellowLabel
 
-func updateBlueCount():
-	blueCount += 1
-	blueCountLabel.text = "%d" % blueCount
+func updateBlueCount(value):
+	blueCount += value
+	blueCountLabel.text = "Blue: %d" % blueCount
 
 
-func updateRedCount():
-	redCount += 1
-	redCountLabel.text = "%d" % redCount
+func updateRedCount(value):
+	redCount += value
+	redCountLabel.text = "Red: %d" % redCount
 
-func updateGreenCount():
-	greenCount += 1
-	greenCountLabel.text = "%d" % greenCount
+func updateGreenCount(value):
+	greenCount += value
+	greenCountLabel.text = "Green: %d" % greenCount
 
-func updateYellowCount():
-	yellowCount += 1
-	yellowCountLabel.text = "%d" % yellowCount
+
+func updateYellowCount(value):
+	yellowCount += value
+	yellowCountLabel.text = "Yellow: %d" % yellowCount
 
 	
 
